9b2b710844d8f031c1d552ab9ab46438191047b4
praveenavulapati <114740842+praveenavulapati@users.noreply.github.com>
2022-11-11 10:04:00 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,46 @@
+
+provider "aws" {
+    region = "us-east-2"
+}
+
+variable "image" {
+  type = string
+  default = "ami-0603cbe34fd08cb81"
+  description = "Enter your APP Image to be deployed"
+}
+
+variable "hw" {
+  type = string
+  default = "t2.nano"
+  description = "Enter your hardware model"
+}
+
+variable "name" {
+  type = string
+  default = "praveen-server1"
+  description = "Enter your server name"
+}
+
+
+
+resource "aws_instance" "myawsserver" {
+  ami = var.image
+  instance_type = var.hw
+
+  tags = {
+    Name = var.name
+    Env = "test"
+    Owner = "praveen"
+  }
+}
+
+
+
+
+output "myawsserver-ip" {
+  value = aws_instance.myawsserver.public_ip
+}
+
+output "myserver-PrivateIP" {
+  value = aws_instance.myawsserver.private_ip
+}
